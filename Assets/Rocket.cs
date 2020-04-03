9f7f6cd7e71b284291120525e6f74593b76d1a1c
Sakurasou2270 <Sakurasou2270@gmail.com>
2020-04-03 04:54:36 -0500

    Fixed Movement & Added Safe and Danger Colliders

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,32 +6,75 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    [SerializeField]
+    float mainThrust = 500f;
+
+    [SerializeField]
+    float rotatingThrust = 100f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProccessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProccessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Hit Friendly");
+                break;
+
+            case "Fuel":
+                print("Hit Fuel");
+                break;
+            default:
+                print("Died");
+                break;
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            print("Pressed A");
+            transform.Rotate(Vector3.forward * rotatingThrust * Time.deltaTime);
         }
+
         if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            print("Pressed D");
+            transform.Rotate(-Vector3.forward * rotatingThrust * Time.deltaTime);
         }
     }
 }
